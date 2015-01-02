8e0f691cdcd2ec42dd86eb5164479f88d015f925
rayray2002 <rayray2002.huang@gmail.com>
2015-01-02 14:36:39 +0800

    1/2

--- meArm.ino/meArm.ino.ino
+++ meArm.ino/meArm.ino.ino
@@ -1,11 +1,29 @@
 #include <Servo.h> 
  
-Servo myservo;
+Servo spin, claw, x, y;
+
 
 void setup() { 
-  myservo.attach(9);
+  spin.attach(7);
+  claw.attach(8);
+  x.attach(9);
+  y.attach(10);
 } 
  
 void loop() {
-  
+  spin.write(180);
+  y.write();
+  x.write();
+  for (int i; i<4; i++) {
+    claw.write(45);
+    delay(200);
+    claw.write(135);
+    delay(200);
+  }
+  delay(1000);
+  claw.write();
+  y.write();
+  spin.write(0);
+  y.write();
+  claw.write();
 } 
