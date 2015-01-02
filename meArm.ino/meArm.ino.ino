40b6682b4837c42b05c416ee5daaf2ef9b915d70
rayray2002 <rayray2002.huang@gmail.com>
2015-01-02 14:51:20 +0800

    1/2 v2

--- meArm.ino/meArm.ino.ino
+++ meArm.ino/meArm.ino.ino
@@ -2,6 +2,10 @@
  
 Servo spin, claw, x, y;
 
+int y_low = 0;
+int y_high = 0;
+int claw_open = 45;
+int claw_close = 135;
 
 void setup() { 
   spin.attach(7);
@@ -12,18 +16,32 @@ void setup() {
  
 void loop() {
   spin.write(180);
-  y.write();
-  x.write();
+  delay(100);
+  y.write(y_low);//low
+  delay(100);
+  x.write(0);//mid
+  delay(100);
   for (int i; i<4; i++) {
-    claw.write(45);
+    claw.write(claw_close);//close
     delay(200);
-    claw.write(135);
+    claw.write(claw_open);//open
     delay(200);
   }
   delay(1000);
-  claw.write();
-  y.write();
+  claw.write(claw_close);//close
+  delay(100);
+  y.write(y_high);//high
+  delay(100);
   spin.write(0);
-  y.write();
-  claw.write();
+  delay(100);
+  y.write(y_low);//low
+  delay(100);
+  claw.write(claw_open);//open
+  delay(100);
+  y.write(y_high);//high
+  delay(100);
+  spin.write(180);
+  delay(100);
+  y.write(y_low);//low
+  delay(100);
 } 
