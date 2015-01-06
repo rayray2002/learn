0f36470d2f0b125758170c3b4034f341197470e9
rayray2002 <rayray2002.huang@gmail.com>
2015-01-06 19:37:26 +0800

    1/6

---
+++ interrupt/button_count_interrupt/button_count_interrupt.ino
@@ -0,0 +1,14 @@
+int count = 0;
+
+void setup() {
+  Serial.begin(9600);
+  attachInterrupt(0, counting, CHANGE);
+}
+void loop() {
+  Serial.println(count);
+  delay(1000);
+}
+
+void counting() {
+  count++;
+}
---
+++ interrupt/button_count_loop/button_count_loop.ino
@@ -0,0 +1,13 @@
+int count = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  if(digitalRead(3) == HIGH) {
+    count++;
+    delay(500);
+  }
+  Serial.println(count);
+}
